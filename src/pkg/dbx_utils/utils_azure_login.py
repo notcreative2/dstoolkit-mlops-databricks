45ffc1824dcc4a0534d08241dd6f3c93bfefae58
notcreative2 <31422899+notcreative2@users.noreply.github.com>
2025-05-01 11:51:39 -0700

    Update utils_azure_login.py

--- src/pkg/dbx_utils/utils_azure_login.py
+++ src/pkg/dbx_utils/utils_azure_login.py
@@ -3,7 +3,8 @@
 """
 import os
 import subprocess
-
+#added pip install
+pip install azure-mgmt-rdbms
 
 ARM_CLIENT_ID = os.environ['ARM_CLIENT_ID']
 ARM_CLIENT_SECRET = os.environ['ARM_CLIENT_SECRET']
